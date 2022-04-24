b371d81c0cac6d7ee71f2630f1575c715113f689
MatterMark97 <hasithapamal@gmail.com>
2022-04-25 01:34:51 +0530

    Create CRUD operation

---
+++ src/main/java/com/electrogrid/client_management/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.electrogrid.client_management;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
